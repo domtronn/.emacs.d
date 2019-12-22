8f3cef0b0f1fcf3cf5917fb1c20c35ac12387d2f
domtronn <dgc336@gmail.com>
2019-12-22 18:07:10 +0000

    Add packages smart-hungry-delete & move-text for editing

--- core/editing.el
+++ core/editing.el
@@ -1,7 +1,15 @@
 ;;; editing --- Tools that make editing files generally easier
 ;;; Commentary:
 ;;; Code:
+(use-package smart-hungry-delete
+  :bind (("<backspace>" . smart-hungry-delete-backward-char)
+         ("C-d" . smart-hungry-delete-forward-char)
+         ("C-S-d" . smart-hungry-delete-backward-char))
+  :config (smart-hungry-delete-add-default-hooks))
 
+(use-package move-text
+  :bind (("s-P" . move-text-up)
+         ("s-N" . move-text-down)))
 
 (provide 'editing)
 ;;; editing.el ends here
