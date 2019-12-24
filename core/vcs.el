26b9ba347fff72b1afa54f31fe2b9f37001fc208
domtronn <dgc336@gmail.com>
2019-12-24 16:46:39 +0000

    Add a bunch of VC packages like todo lists and ediffing

--- core/vcs.el
+++ core/vcs.el
@@ -26,9 +26,33 @@
               ("t" . git-timemachine)))
 
 (use-package browse-at-remote
-  :bind (("C-c v" . browse-at-remote)
+  :bind (:map vc-prefix-map
+         ("v" . browse-at-remote)
          :map magit-mode-map
          ("V" . browse-at-remote)))
+
+(use-package git-link
+  :bind (:map vc-prefix-map
+              ("l" . git-link)))
+
+(use-package ediff
+  :ensure nil
+  :init (defvar ediff-window-config nil)
+  :bind (:map vc-prefix-map ("d" . vc-ediff))
+  :hook ((ediff-before-setup
+          . (lambda () (setq ediff-window-config (current-window-configuration))))
+         (ediff-quit
+          . (lambda ()
+              (set-window-configuration ediff-window-config)
+              (-map 'kill-buffer
+                        (--filter (and (not (string-equal (buffer-name) (buffer-name it)))
+                                       (s-starts-with-p (buffer-name) (buffer-name it)))
+                                  (buffer-list))))))
+  :config
+  (setq
+   ediff-keep-variants t
+   ediff-split-window-function 'split-window-horizontally
+   ediff-window-setup-function 'ediff-setup-windows-plain))
 
 (provide 'vcs)
 
--- custom.el
+++ custom.el
@@ -16,7 +16,7 @@
  '(ns-function-modifier (quote hyper))
  '(package-selected-packages
    (quote
-    (mwim avy-flycheck avy-jump tide visual-regexp-steroids go-fill-struct electric-operator embrace auto-rename-tag rjsx-mode js2-mode json-mode counsel-projectile smartparens-config smartparens move-text quickrun smart-hungry-delete format-all whitespace-cleanup-mode winum uniquify lsp-ui flycheck-title flycheck-inline flycheck company-lsp company-box company-posframe company-quickhelp counsel-osx-app amx ivy-rich ivy-prescient ivy-posframe ivy-posframe-font company-prescient treemacs-projectile treemacs-magit treemacs magit projectile ivy-hydra all-the-icons highlight-parentheses highlight-quoted highlight-defined go-eldoc go-mode lsp-mode counsel smex company exec-path-from-shell dashboard esup use-package)))
+    (git-link git-timemachine magit-todos git-messenger browse-at-remote duplicate-thing whole-line-or-region shackle symbol-overlay smart-newline eros highlight-symbol company-emoji comapny-emoji spu forge dumb-jump mwim avy-flycheck avy-jump tide visual-regexp-steroids go-fill-struct electric-operator embrace auto-rename-tag rjsx-mode js2-mode json-mode counsel-projectile smartparens-config smartparens move-text quickrun smart-hungry-delete format-all whitespace-cleanup-mode winum uniquify lsp-ui flycheck-title flycheck-inline flycheck company-lsp company-box company-posframe company-quickhelp counsel-osx-app amx ivy-rich ivy-prescient ivy-posframe ivy-posframe-font company-prescient treemacs-projectile treemacs-magit treemacs magit projectile ivy-hydra all-the-icons highlight-parentheses highlight-quoted highlight-defined go-eldoc go-mode lsp-mode counsel smex company exec-path-from-shell dashboard esup use-package)))
  '(ring-bell-function (quote ignore))
  '(scroll-bar-mode nil)
  '(tab-width 2)
@@ -28,6 +28,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
+ '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
+ '(symbol-overlay-default-face ((t (:inherit (region bold))))))
 
 (provide 'custom)
