b2e6ff66407e45cc628f67dc8b23e514ca278ab5
domtronn <dgc336@gmail.com>
2019-12-27 12:58:39 +0000

    Add bindings to go mode libraries

--- modes/go.el
+++ modes/go.el
@@ -1,3 +1,10 @@
+;;; go.el --- Go specific configuration
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
 (use-package go-mode
   :mode "\\.go$"
   :bind (:map go-mode-map
@@ -7,7 +14,25 @@
   :after go-mode
   :hook (go-mode . go-eldoc-setup))
 
+(use-package go-tag
+  :after go-mode
+  :bind (:map go-mode-map
+              ("C-c t" . go-tag-add)
+              ("C-c T" . go-tag-remove))
+  :config (setq go-tag-args (list "-transform" "camelcase"))
+  )
+
 (use-package go-fill-struct
-  :after go-mode)
+  :after go-mode
+  :bind (:map go-mode-map
+              ("M-RET"     . go-fill-struct)
+              ("C-c f" . go-fill-struct))
+  :config
+  (advice-add 'go-fill-struct :after 'gofmt))
+
+(use-package go-rename :bind ("C-c r" . go-rename))
+(use-package go-impl :bind ("C-c C-c i" . go-impl))
 
 (provide 'go)
+
+;;; go.el ends here
