8977cc76a34733a2d311c23b6c0678572f9277bc
domtronn <dgc336@gmail.com>
2019-12-27 11:02:25 +0000

    Add function to define thing at point

--- modes/elisp.el
+++ modes/elisp.el
@@ -29,5 +29,31 @@
 (use-package auto-compile
   :hook (emacs-lisp-mode . auto-compile-on-save-mode))
 
+(defun describe-foo-at-point ()
+  "Show the documentation of the Elisp function and variable near point.
+
+This checks in turn:
+	-- for a function name where point is
+	-- for a variable name where point is
+	-- for a surrounding function call"
+  (interactive)
+  (let (sym)
+    ;; sigh, function-at-point is too clever.  we want only the first half.
+    (cond ((setq sym (ignore-errors
+                       (with-syntax-table emacs-lisp-mode-syntax-table
+                         (save-excursion
+                           (or (not (zerop (skip-syntax-backward "_w")))
+                               (eq (char-syntax (char-after (point))) ?w)
+                               (eq (char-syntax (char-after (point))) ?_)
+                               (forward-sexp -1))
+                           (skip-chars-forward "`'")
+                           (let ((obj (read (current-buffer))))
+                             (and (symbolp obj) (fboundp obj) obj))))))
+           (describe-function sym))
+          ((setq sym (variable-at-point)) (describe-variable sym))
+          ;; now let it operate fully -- i.e. also check the
+          ;; surrounding sexp for a function call.
+          ((setq sym (function-at-point)) (describe-function sym)))))
+
 (provide 'elisp)
 ;;; elisp.el ends here
